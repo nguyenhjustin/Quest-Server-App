a13f7d29bb791aaa34d297a594314fe422a69a17
Justin Nguyen <nguyenhjustin@yahoo.com>
2018-07-24 21:24:03 -0700

    Add tests

--- app/test/test.js
+++ app/test/test.js
@@ -15,9 +15,10 @@ after(function() {
 });
 
 describe('quest server backend tests', function () {
+  let playerId1 = "playerId1";
 
   it('update progress', function(done) {
-    var options = GetOptionsForUpdateProgress("id1", 2, 10);
+    let options = GetOptionsForUpdateProgress(playerId1, 2, 10);
     request.post(options, function(err, res, body) {
       expect(res.statusCode).to.equal(200);
       console.log(body);
@@ -25,11 +26,30 @@ describe('quest server backend tests', function () {
     });
   });
 
+  // it('update progress quest already complete', function(done) {
+  //   let options = GetOptionsForUpdateProgress(playerId1, 2, 10);
+  //   request.post(options, function(err, res, body) {
+  //     expect(res.statusCode).to.equal(200);
+  //     console.log(body);
+  //     done();
+  //   });
+  // });
+
   it('get state', function(done) {
-    var options = GetOptionsForGetState("id1");
+    let options = GetOptionsForGetState(playerId1);
     request.get(options, function(err, res, body) {
       expect(res.statusCode).to.equal(200);
-      console.log(body);
+      expect(body.TotalQuestPercentCompleted).to.equal(26);
+      expect(body.LastMilestoneIndexCompleted).to.equal(1);
+      done();
+    });
+  });
+
+  it("get state playerId doesn't exist", function(done) {
+    let options = GetOptionsForGetState("doesNotExistId");
+    request.get(options, function(err, res, body) {
+      expect(res.statusCode).to.equal(500);
+      console.log(body.error);
       done();
     });
   });
